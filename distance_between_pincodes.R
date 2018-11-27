fd11e1e2c0ac3a18866ecfcdd8a0673b8fb09b9b
Thulasi <2004175+thulasirangan@users.noreply.github.com>
2018-11-27 15:45:38 +0530

    Update distance_between_pincodes.R

--- distance_between_pincodes.R
+++ distance_between_pincodes.R
@@ -2,9 +2,10 @@ install.packages("mapsapi")
 library(mapsapi)
 
 doc = mp_matrix(
-  origins = '577201',
-  destinations = '635126', key = '<API KEY>'
+  origins = '85001',
+  destinations = '99501', key = 'AIzaSyCoF_GdEyytZPRS852RRNJdAHHQ8-IuJOk'
 )
+#get the API key from GCP
 
 m = mp_get_matrix(doc, value = "distance_m")
 m[1] #distance in meters
